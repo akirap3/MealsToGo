b7cb5990a1fb0d5a26e2355cd9fda094a4ceab62
akirap3 <akirapf3@gmail.com>
2022-08-12 00:03:13 +0800

    Move style to StyleSheet

--- App.js
+++ App.js
@@ -12,17 +12,21 @@ import {
 export default function App() {
   return (
     <>
-      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
-        <View style={{ padding: 16, backgroundColor: 'green' }}>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
           <Text>search</Text>
         </View>
-        <View style={{ flex: 1, padding: 16, backgroundColor: 'cyan' }}>
+        <View style={styles.list}>
           <Text>list</Text>
         </View>
       </SafeAreaView>
-      <StatusBar style="auto" />
+      <ExpoStatusBar style="auto" />
     </>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: { flex: 1, marginTop: StatusBar.currentHeight },
+  search: { padding: 16, backgroundColor: 'green' },
+  list: { flex: 1, padding: 16, backgroundColor: 'cyan' },
+});
